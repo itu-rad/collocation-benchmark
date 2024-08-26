9d122a07e4990ca5af35659fa2e1eb3f429e3082
Robert Bayer <roba@itu.dk>
2024-08-26 16:02:12 +0200

    Enabled preloading of the dataset

--- stages/dataset/torchvision_dataset.py
+++ stages/dataset/torchvision_dataset.py
@@ -2,9 +2,25 @@ import os
 import torchvision.datasets
 from torchvision.transforms import v2
 from torchvision.models import get_weight
+from torchvision.datasets import VisionDataset
 
 
-from stages.stage import Stage, log_phase_single
+from stages.stage import Stage, log_phase, log_phase_single
+
+
+class PreloadedDataset(VisionDataset):
+    def __init__(self, dataset, transform=None):
+        self.transform = transform
+        self.data = []
+        for idx in range(len(dataset)):
+            self.data.append(dataset[idx])
+
+    def __getitem__(self, index):
+        x, y = self.data[index]
+        return self.transform(x), y
+
+    def __len__(self):
+        return len(self.data)
 
 
 class TorchVisionDataset(Stage):
@@ -26,6 +42,8 @@ class TorchVisionDataset(Stage):
         super().__init__(stage_config, parent_name)
         stage_config = stage_config.get("config", {})
 
+        self.preload = stage_config.get("preload", False)
+
         dataset_name = stage_config.get("dataset_name", None)
         if dataset_name is None:
             raise ValueError("Missing dataset name.")
@@ -39,7 +57,7 @@ class TorchVisionDataset(Stage):
         # print(f"Dataset downloaded? {dataset_downloaded}")
         weights_name = stage_config.get("weights", None)
         if weights_name is None:
-            self.datasets = {
+            self.datasets: dict[str, VisionDataset] = {
                 x: self.dataset(
                     root=f"data/{dataset_name}",
                     split=x,
@@ -51,7 +69,7 @@ class TorchVisionDataset(Stage):
         else:
             weights = get_weight(weights_name)
             preprocess = weights.transforms()
-            self.datasets = {
+            self.datasets: dict[str, VisionDataset] = {
                 x: self.dataset(
                     root=f"data/{dataset_name}",
                     split=x,
@@ -78,6 +96,22 @@ class TorchVisionDataset(Stage):
             dict[str, int]: dictionary with number of batches for each dataset
         """
         return {k: len(v) // self.batch_size for (k, v) in self.datasets.items()}
+
+    @log_phase
+    def prepare(self):
+        """Preload dataset (specifically useful for modelling inference)"""
+        super().prepare()
+
+        if not self.preload:
+            return
+
+        for key in self.datasets.keys():
+            # get the old transform
+            transform = self.datasets[key].transform
+            # remove transform from preloading
+            self.datasets[key].transform = None
+            # add transform to cached dataset
+            self.datasets[key] = PreloadedDataset(self.datasets[key], transform)
 
     def run(self):
         """Pass the input onto the output (no action to be performed on the data)"""
