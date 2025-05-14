f399e7658c5bfaf236591a99212837296f6f3d8c
sipondo <titr@itu.dk>
2025-05-14 11:08:25 +0000

    run via radt via schedule_external

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import argparse
+
+import radt
+
+import numpy as np
+import pandas as pd
 from multiprocessing import Process, Queue
 from pydantic_yaml import parse_yaml_raw_as
 
@@ -14,12 +19,24 @@ def parse_args():
         type=str,
         help="path to the pipeline configuration file.",
     )
+    parser.add_argument(
+        "-p",
+        type=int,
+        dest="pipeline_id",
+        default=-1,
+        # help="Maximum amount of time to train for (minutes)",
+    )
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
+def radt_entrypoint(args):
+    with open(args.config_file_path, "r", encoding="utf-8") as file:
+        yaml_config = file.read()
+        benchmark_config = parse_yaml_raw_as(BenchmarkModel, yaml_config)
+    run_loadgen(benchmark_config.pipelines[args.pipeline_id])
 
+
+def main(args):
     with open(args.config_file_path, "r", encoding="utf-8") as file:
         yaml_config = file.read()
         benchmark_config = parse_yaml_raw_as(BenchmarkModel, yaml_config)
@@ -28,22 +45,34 @@ def main():
     logger_queue = Queue()
     logger = Logger(logger_queue, benchmark_config.name)
 
-    # start each loadgen/pipeline as a separate process
-    loadgen_processes = [
-        Process(target=run_loadgen, args=[pipeline_config])
-        for pipeline_config in benchmark_config.pipelines
-    ]
+    # build the radt schedule
+    pipeline_ids = [i for i in range(len(benchmark_config.pipelines))]
+    df_schedule = pd.DataFrame(np.empty(0, dtype=radt.constants.CSV_FORMAT))
+    for pipeline_id in pipeline_ids:
+        df_schedule.loc[pipeline_id] = pd.Series(
+            {
+                "Experiment": 0,
+                "Workload": 0,
+                "Status": "",
+                "Run": "",
+                "Devices": 0,
+                "Collocation": "",
+                "Listeners": "smi",
+                "File": "main.py",
+                "Params": f"{args.config_file_path} -p {pipeline_id}",
+            }
+        )
 
-    # start child processes
-    for loadgen_process in loadgen_processes:
-        loadgen_process.start()
+    # execute workload
+    radt.schedule_external(["--local"], df_schedule)
 
-    # wait for child processes to finish
-    for loadgen_process in loadgen_processes:
-        loadgen_process.join()
-
+    # stop the logger
     logger.stop_queue_listener()
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    if args.pipeline_id != -1:
+        radt_entrypoint(args)
+    else:
+        main(args)
