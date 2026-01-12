e93592a73cdc83ce68db4bc2d1e5b89e5bcec47f
Titus Robroek <ties@trobroek.com>
2026-01-12 15:17:23 +0100

    Feat/sipondo/radt support (#4)
    Basic radT Support for the benchmarking framework
    
    ---------
    
    Co-authored-by: Robert Bayer <roba@itu.dk>

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import argparse
+import mlflow
+import sys
+
+import radt
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel
 from multiprocessing import Process, Queue
 from pydantic_yaml import parse_yaml_raw_as
 
@@ -14,12 +22,58 @@ def parse_args():
         type=str,
         help="path to the pipeline configuration file.",
     )
+    parser.add_argument(
+        "-p",
+        type=int,
+        dest="pipeline_id",
+        default=-1,
+        # help="Maximum amount of time to train for (minutes)",
+    )
+    parser.add_argument(
+        "-e",
+        type=int,
+        dest="experiment_id",
+        default=0,
+        help="radT experiment id",
+    )
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
+def radt_entrypoint(args):
+    with open(args.config_file_path, "r", encoding="utf-8") as file:
+        yaml_config = file.read()
+        benchmark_config = parse_yaml_raw_as(BenchmarkModel, yaml_config)
 
+        # Parse the .yaml and send it over as mlflow params
+        def build_mlflow_config(
+            config: dict, data: BaseModel | list | dict, directory: str
+        ) -> None:
+            if isinstance(data, BaseModel):
+                for k in data.model_fields_set:
+                    v = getattr(data, k)
+                    build_mlflow_config(config, v, f"{directory}.{k}")
+            elif isinstance(data, list):
+                for i, v in enumerate(data):
+                    build_mlflow_config(config, v, f"{directory}:{i}")
+            elif isinstance(data, dict):
+                for k, v in data.items():
+                    build_mlflow_config(config, v, f"{directory}.{k}")
+            else:
+                config[f"{directory}"] = data
+
+        # Log config
+        mlflow.log_artifact(args.config_file_path, "pipeline")
+
+        mlflow_config = {}
+        build_mlflow_config(
+            mlflow_config, benchmark_config.pipelines[args.pipeline_id], "pipeline"
+        )
+        mlflow.log_params(mlflow_config)
+
+    run_loadgen(benchmark_config.pipelines[args.pipeline_id])
+
+
+def main(args):
     with open(args.config_file_path, "r", encoding="utf-8") as file:
         yaml_config = file.read()
         benchmark_config = parse_yaml_raw_as(BenchmarkModel, yaml_config)
@@ -28,22 +82,39 @@ def main():
     logger_queue = Queue()
     logger = Logger(logger_queue, benchmark_config.name)
 
-    # start each loadgen/pipeline as a separate process
-    loadgen_processes = [
-        Process(target=run_loadgen, args=[pipeline_config])
-        for pipeline_config in benchmark_config.pipelines
-    ]
+    # build the radt schedule
+    pipeline_ids = [i for i in range(len(benchmark_config.pipelines))]
+    df_schedule = pd.DataFrame(np.empty(0, dtype=radt.constants.CSV_FORMAT))
+    for pipeline_id in pipeline_ids:
+        df_schedule.loc[pipeline_id] = pd.Series(
+            {
+                "Experiment": args.experiment_id,
+                "Workload": 0,
+                "Name": benchmark_config.pipelines[pipeline_id].name,
+                "Status": "",
+                "Run": "",
+                "Devices": 0,
+                "Collocation": "",
+                "Listeners": "smi+top+dcgmi+iostat+free",
+                "File": "main.py",
+                "Params": f"{args.config_file_path} -p {pipeline_id}",
+            }
+        )
 
-    # start child processes
-    for loadgen_process in loadgen_processes:
-        loadgen_process.start()
+    # execute workload
+    radt.schedule_external(
+        [],
+        df_schedule,
+        group_name=benchmark_config.name,
+    )
 
-    # wait for child processes to finish
-    for loadgen_process in loadgen_processes:
-        loadgen_process.join()
-
+    # stop the logger
     logger.stop_queue_listener()
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    if args.pipeline_id != -1:
+        radt_entrypoint(args)
+    else:
+        main(args)
